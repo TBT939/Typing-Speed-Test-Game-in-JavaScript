e63cc87dca26fee82328693942e209f58f9b71fa
TBT939 <Sigurd.nattestad.thomsen@gmail.com>
2024-04-24 20:12:12 +0100

    +

--- js/script1.js
+++ js/script1.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
   startButton.addEventListener('click', function() {
     window.location.href = "typing.html";
   });
+
+  const blankPageButtons = document.querySelectorAll('.blankPageButton');
+  blankPageButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const page = button.getAttribute('data-page');
+      window.open(page);
+    });
+  });
 });
